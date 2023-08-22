acdfab1679db866ed95667213dfb2073d1e8a061
metr-0 <mar2017ar@gmail.com>
2023-08-23 00:07:23 +0300

    add docker-compose

--- app/personal_site/urls.py
+++ app/personal_site/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.static import serve
+from django.urls import path, include
 
 from personal_site import settings
 
@@ -12,8 +11,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += [
-        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
-    ]
